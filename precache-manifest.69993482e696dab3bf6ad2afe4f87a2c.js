a05db2f8cdc842a6ede77eb2589b27b2f39aab20
Niscolinx <munisco12@gmail.com>
2019-10-02 22:28:25 +0100

    Updates

---
+++ precache-manifest.69993482e696dab3bf6ad2afe4f87a2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac51bced2b06f1c4aa5d0c79210fdd4",
+    "url": "/24movies/index.html"
+  },
+  {
+    "revision": "b3159a08ee1b7da12069",
+    "url": "/24movies/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "fc98ad508702b121e37b",
+    "url": "/24movies/static/js/2.cdcaafab.chunk.js"
+  },
+  {
+    "revision": "b3159a08ee1b7da12069",
+    "url": "/24movies/static/js/main.232889ee.chunk.js"
+  },
+  {
+    "revision": "72a1b2324263ba265867",
+    "url": "/24movies/static/js/runtime-main.64403add.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/24movies/static/media/logo.25bf045c.svg"
+  }
+]);
